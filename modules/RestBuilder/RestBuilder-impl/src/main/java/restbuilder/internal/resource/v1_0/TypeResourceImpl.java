9e18301be5e65e3c18c9302305a15415bfeec34c
wesleyroberts <wesley.roberts@liferay.com>
2021-11-22 13:14:19 -0300

    implemented typeResource Method's

--- modules/RestBuilder/RestBuilder-impl/src/main/java/restbuilder/internal/resource/v1_0/TypeResourceImpl.java
+++ modules/RestBuilder/RestBuilder-impl/src/main/java/restbuilder/internal/resource/v1_0/TypeResourceImpl.java
@@ -1,9 +1,25 @@
 package restbuilder.internal.resource.v1_0;
 
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.sales.model.SaleType;
+import com.liferay.sales.service.SaleTypeService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
+import restbuilder.dto.v1_0.Type;
 import restbuilder.resource.v1_0.TypeResource;
+
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Wesley Roberts
@@ -13,4 +29,95 @@ import restbuilder.resource.v1_0.TypeResource;
 	scope = ServiceScope.PROTOTYPE, service = TypeResource.class
 )
 public class TypeResourceImpl extends BaseTypeResourceImpl {
+	/**
+	 * Invoke this method with the command line:
+	 *
+	 * curl -X 'GET' 'http://localhost:8080/o/RestBuilder/v1.0/type/all'  -u 'test@liferay.com:test'
+	 */
+	@GET
+	@Override
+	@Path("/type/all")
+	@Produces({"application/json", "application/xml"})
+	@Tags(value = {@Tag(name = "Type")})
+	public Page<Type> getTypesAllPage() throws Exception {
+		List<Type> listTypeDTO = new ArrayList<Type>();
+		for(SaleType e: _saleTypeService.getAll()){
+			listTypeDTO.add(_toTypeDTO(e));
+		}
+		return Page.of(listTypeDTO);
+	}
+
+	/**
+	 * Invoke this method with the command line:
+	 *
+	 * curl -X 'GET' 'http://localhost:8080/o/RestBuilder/v1.0/type/{typeId}'  -u 'test@liferay.com:test'
+	 */
+	@GET
+	@Override
+	@Parameters(value = {@Parameter(in = ParameterIn.PATH, name = "typeId")})
+	@Path("/type/{typeId}")
+	@Produces({"application/json", "application/xml"})
+	@Tags(value = {@Tag(name = "Type")})
+	public Type getType(
+			@NotNull @Parameter(hidden = true) @PathParam("typeId") Integer
+					typeId)
+			throws Exception {
+
+		SaleType saleType = _saleTypeService.getSaleTypeByID(typeId);
+		return _toTypeDTO(saleType);
+
+	}
+
+	/**
+	 * Invoke this method with the command line:
+	 *
+	 * curl -X 'POST' 'http://localhost:8080/o/RestBuilder/v1.0/type/post' -d $'{"id": ___, "name": ___, "tax": ___}' --header 'Content-Type: application/json' -u 'test@liferay.com:test'
+	 */
+	@Consumes({"application/json", "application/xml"})
+	@Override
+	@Path("/type/post")
+	@POST
+	@Produces({"application/json", "application/xml"})
+	@Tags(value = {@Tag(name = "Type")})
+	public Type postTypePost(Type type) throws Exception {
+		SaleType saleType = _saleTypeService.
+				createSaleType(
+						type.getId(),
+						type.getName(),
+						type.getTax());
+
+		return _toTypeDTO(saleType);
+	}
+
+	/**
+	 * Invoke this method with the command line:
+	 *
+	 * curl -X 'DELETE' 'http://localhost:8080/o/RestBuilder/v1.0/type/delete/{typeId}'  -u 'test@liferay.com:test'
+	 */
+	@DELETE
+	@Override
+	@Parameters(value = {@Parameter(in = ParameterIn.PATH, name = "typeId")})
+	@Path("/type/delete/{typeId}")
+	@Produces({"application/json", "application/xml"})
+	@Tags(value = {@Tag(name = "Type")})
+	public void deleteTypeDeleteType(
+			@NotNull @Parameter(hidden = true) @PathParam("typeId") Integer
+					typeId)
+			throws Exception {
+		_saleTypeService.deleteTypeById(typeId);
+	}
+
+	private Type _toTypeDTO(SaleType saleType) {
+		return new Type(){
+			{
+				id = (int)saleType.getTypeId();
+				name = saleType.getName();
+				tax = saleType.getTax();
+			}
+		};
+	}
+
+	@Reference
+	SaleTypeService _saleTypeService;
+
 }
