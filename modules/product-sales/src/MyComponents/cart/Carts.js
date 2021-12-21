79dba013a4226942e1a12083074a8498ac4e1caf
Jamerson Souza <jamersonze54@gmail.com>
2021-12-21 15:50:28 -0300

    Implementing customAlert at Create and Delete cart in Cart.js

--- modules/product-sales/src/MyComponents/cart/Carts.js
+++ modules/product-sales/src/MyComponents/cart/Carts.js
@@ -9,12 +9,16 @@ import {
 } from "../../resourceRequests/CartFunctions";
 import RemoveProductsModal from "./modal/RemoveProductsModal";
 import FinishedBuyModal from "./modal/FinishedBuyModal";
+import { Alert } from "../alert/CustomAlert"
+
 export default function Carts({ cartList, addCart, deleteCart }) {
   const [showRemoveproductsModal, setShowRemoveproductsModal] = useState(false);
   const [cartIdSelected, setCartIdSelected] = useState([]);
   const [showFinishedBuyModal, setShowFinishedBuyModal] = useState(false);
   const [productsInCartList, setProductsInCartList] = useState([]);
   const [totalValue, setTotalValue] = useState(0)
+  const [alertAddtoCartSuccess, setAlertToCartSuccess] = useState(false)
+  const [alertDeleteSuccess, setAlertDeleteSuccess] = useState(false)
 
   const removeProductOfList = (id) => {
     var res = [];
@@ -40,6 +44,32 @@ export default function Carts({ cartList, addCart, deleteCart }) {
       setTotalValue(data)
     })
     setShowFinishedBuyModal(true);
+  }
+
+  const handleCartCreate = () => {
+    CreateCart().then((response) => {
+      if(!response) {
+        console.log(response)
+        throw new Error(response.status)}
+      else {
+        addCart(response)
+        setAlertToCartSuccess(true)
+      }
+    })
+    .catch((error) => console.log("error:", error));
+  }
+
+  const handleCartDelete = (id) => {
+    DeleteCartByID(id).then((response) => {
+      if(!response.ok) {
+        console.log(response)
+        throw new Error(response.status)}
+      else {
+        deleteCart(id)
+        setAlertDeleteSuccess(true)
+      }
+    })
+    .catch((error) => console.log("error:", error));
   }
 
   return (
@@ -79,11 +109,8 @@ export default function Carts({ cartList, addCart, deleteCart }) {
                     },
                     { type: "divider" },
                     {
-                      href: "#",
                       label: "Delete",
-                      onClick: () => {
-                        DeleteCartByID(item.id), deleteCart(item.id);
-                      },
+                      onClick: () => handleCartDelete(item.id),
                     },
                   ]}
                   title={item.id}
@@ -93,14 +120,22 @@ export default function Carts({ cartList, addCart, deleteCart }) {
           </div>
           <ClayButton
             displayType="primary"
-            onClick={() => {
-              CreateCart().then((data) => addCart(data));
-            }}
+            onClick={handleCartCreate}
           >
             Create Cart
           </ClayButton>
         </ClayCard.Body>
       </ClayCard>
+      {alertAddtoCartSuccess && (
+            <Alert displayType={"success"} title={"Success: "} message={"The cart were successfully created"}>
+              {setTimeout(() => setAlertToCartSuccess(false), 5000)}
+            </Alert>
+      )}
+      {alertDeleteSuccess && (
+            <Alert displayType={"success"} title={"Success: "} message={"The cart were successfully deleted"}>
+              {setTimeout(() => setAlertDeleteSuccess(false), 5000)}
+            </Alert>
+      )}
     </div>
   );
 }
