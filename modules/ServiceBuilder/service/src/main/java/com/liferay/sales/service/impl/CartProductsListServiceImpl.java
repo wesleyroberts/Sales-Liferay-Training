275ed6f39b804d528015962c1b22865278b86be9
wesleyroberts <wesley.roberts@liferay.com>
2022-01-03 15:25:31 -0300

    refactoring CartProductList Remote and Local Service

--- modules/ServiceBuilder/service/src/main/java/com/liferay/sales/service/impl/CartProductsListServiceImpl.java
+++ modules/ServiceBuilder/service/src/main/java/com/liferay/sales/service/impl/CartProductsListServiceImpl.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
+ * <p>
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- *
+ * <p>
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -38,37 +38,41 @@ import java.util.List;
  * @see CartProductsListServiceBaseImpl
  */
 @Component(
-	property = {
-		"json.web.service.context.name=salestaxe",
-		"json.web.service.context.path=CartProductsList"
-	},
-	service = AopService.class
+        property = {
+                "json.web.service.context.name=salestaxe",
+                "json.web.service.context.path=CartProductsList"
+        },
+        service = AopService.class
 )
 public class CartProductsListServiceImpl
-	extends CartProductsListServiceBaseImpl {
+        extends CartProductsListServiceBaseImpl {
 
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use <code>com.liferay.sales.service.CartProductsListServiceUtil</code> to access the cart products list remote service.
-	 */
-	public List<SaleProduct> getAllProductsByCarID(long id){
-		return cartProductsListLocalService.getAllProductsByCartID(id);
-	}
-	public List<SaleProduct> addProductToCartList( int quantity,long cartId, long stockId){
-		return cartProductsListLocalService.addProductToCartList(quantity,cartId,stockId);
-	}
-	public void removeProductToCartList(long cartId, long productId){
-		cartProductsListLocalService.removeProductToCartList(cartId,productId);
-	}
-	public List<CartProductsList> getAllCartProductsList(){
-		return cartProductsListLocalService.getAllCartProductsList();
-	}
+    /*
+     * NOTE FOR DEVELOPERS:
+     *
+     * Never reference this class directly. Always use <code>com.liferay.sales.service.CartProductsListServiceUtil</code> to access the cart products list remote service.
+     */
+    public List<SaleProduct> getAllProductsByCarID(long id) {
+        return cartProductsListLocalService.getAllProductsByCartID(id);
+    }
 
-	public void deleteCartProductsList(long productId) throws PortalException {
-		  cartProductsListLocalService.deleteCartList(productId);
-	}
-	public SaleCart finishCart(long cartId){
-		return cartProductsListLocalService.finishCart(cartId);
-	}
+    public List<SaleProduct> addProductToCartList(int quantity, long cartId, long stockId) {
+        return cartProductsListLocalService.addProductToCartList(quantity, cartId, stockId);
+    }
+
+    public void removeProductToCartList(long cartId, long productId) {
+        cartProductsListLocalService.removeProductToCartList(cartId, productId);
+    }
+
+    public List<CartProductsList> getAllCartProductsList() {
+        return cartProductsListLocalService.getAllCartProductsList();
+    }
+
+    public void deleteCartProductsList(long productId) throws PortalException {
+        cartProductsListLocalService.deleteCartList(productId);
+    }
+
+    public SaleCart finishCart(long cartId) {
+        return cartProductsListLocalService.finishCart(cartId);
+    }
 }
